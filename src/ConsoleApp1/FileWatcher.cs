37c2ed533846cea00503df70b8097a98d50fda4f
SSenshi147 <47696835+SSenshi147@users.noreply.github.com>
2025-12-07 23:03:47 +0100

    added filewatcher

---
+++ src/ConsoleApp1/FileWatcher.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1;
+
+public static class FileWatcher
+{
+    public static void CheckForChanges()
+    {
+        Console.WriteLine("Hello, World!");
+
+        using var watcher = new FileSystemWatcher(@"C:\Users\MARCI\Documents\test", "*.txt");
+        watcher.Changed += Watcher_Changed;
+        watcher.EnableRaisingEvents = true;
+
+        while (true)
+        {
+            watcher.WaitForChanged(WatcherChangeTypes.Changed);
+        }
+    }
+
+    private static void Watcher_Changed(object sender, FileSystemEventArgs e)
+    {
+        using var fileStream = File.OpenRead(@"C:\Users\MARCI\Documents\test\asd.txt");
+        using var sr = new StreamReader(fileStream);
+        int sum = 0;
+        while (!sr.EndOfStream)
+        {
+            if (int.TryParse(sr.ReadLine(), out var add))
+                sum += add;
+        }
+        Console.WriteLine(sum);
+    }
+}
--- src/WebApplication1/Program.cs
+++ src/WebApplication1/Program.cs
@@ -1,16 +1,24 @@
 
+using Serilog;
+
 namespace WebApplication1;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
         var builder = WebApplication.CreateBuilder(args);
 
         // default
         builder.Services.AddControllers();
 
         builder.Services.SetupSwagger();
+
+        builder.Services.AddSerilog();
 
         var app = builder.Build();
 
