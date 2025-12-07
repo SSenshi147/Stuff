7bd634e0d57b3155b593ec9aceadd77314dc4a08
SSenshi147 <47696835+SSenshi147@users.noreply.github.com>
2025-12-07 23:12:29 +0100

    added file create

---
+++ src/ConsoleApp1/FileCreate.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1;
+
+public static class FileCreate
+{
+    public static void Create()
+    {
+        const string path = "out.txt";
+        if (!File.Exists(path))
+        {
+            // add the using here, otherwise the stream returned by the Create method will block operations
+            using var _ = File.Create(path);
+        }
+
+        File.WriteAllText(path, "test");
+        var content = File.ReadAllText(path);
+
+        Console.WriteLine(content);
+    }
+}
