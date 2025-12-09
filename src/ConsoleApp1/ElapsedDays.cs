f6710d7d3011e038091075b371a6bad1ccf147d0
SSenshi147 <47696835+SSenshi147@users.noreply.github.com>
2025-12-09 21:02:45 +0100

    added

---
+++ src/ConsoleApp1/ElapsedDays.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1;
+
+/// <summary>
+/// I have no idea what was the point of this.
+/// </summary>
+public static class ElapsedDays
+{
+    public static void Main(DateTime end, DateTime begin)
+    {
+        Console.WriteLine((end - begin).Days);
+    }
+}
---
+++ src/ConsoleApp1/TryCatching.cs
@@ -0,0 +1,98 @@
+﻿namespace ConsoleApp1;
+
+public static class TryCatching
+{
+    static bool doThrow = false;
+
+    public static void Main(bool doThrow)
+    {
+        TryCatching.doThrow = doThrow;
+        With();
+        WithFail();
+        Without();
+        WithoutFail();
+    }
+
+    static void MightThrow()
+    {
+        if (doThrow)
+            throw new MyException();
+    }
+
+    static void With()
+    {
+        try
+        {
+            Console.WriteLine("try");
+            MightThrow();
+            return;
+        }
+        catch (MyException)
+        {
+            Console.WriteLine("catch");
+        }
+        finally
+        {
+            Console.WriteLine("finally");
+        }
+
+        Console.WriteLine("after");
+    }
+
+    static void WithFail()
+    {
+        try
+        {
+            Console.WriteLine("try");
+            MightThrow();
+            return;
+        }
+        catch (WrongException)
+        {
+            Console.WriteLine("catch");
+        }
+        finally
+        {
+            Console.WriteLine("finally");
+        }
+
+        Console.WriteLine("after");
+    }
+
+    static void Without()
+    {
+        try
+        {
+            Console.WriteLine("try");
+            MightThrow();
+            return;
+        }
+        catch (MyException)
+        {
+            Console.WriteLine("catch");
+        }
+
+        Console.WriteLine("after");
+    }
+
+    static void WithoutFail()
+    {
+        try
+        {
+            Console.WriteLine("try");
+            MightThrow();
+            return;
+        }
+        catch (WrongException)
+        {
+            Console.WriteLine("catch");
+        }
+
+
+        Console.WriteLine("after");
+    }
+}
+
+
+class MyException : Exception { }
+class WrongException : Exception { }
