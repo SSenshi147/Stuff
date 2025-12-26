0f52befe29df87df83af61934efbd17f5dad1671
SSenshi147 <47696835+SSenshi147@users.noreply.github.com>
2025-12-26 15:19:39 +0100

    Update Program.cs

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -4,6 +4,28 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(AdventOfCode2025._01Safe.Code);
+        for (int i = 0; i < 101; i++)
+        {
+            if (IsPrime(i))
+                Console.WriteLine(i);
+        }
+    }
+
+    /// <summary>
+    /// JS devs download a package for this.
+    /// </summary>
+    static bool IsPrime(int inputNumber)
+    {
+        var sqrt = Math.Sqrt(inputNumber);
+
+        for (int i = 2; i <= sqrt; i++)
+        {
+            if (inputNumber % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
